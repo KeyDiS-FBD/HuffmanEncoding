d15684d3f654d11eca6211845eece2d4274a6fec
KeyDiS <KeyDiS-FBD@github.com>
2020-05-05 20:22:34 +0600

    full worked version

--- huffman.py
+++ huffman.py
@@ -1,13 +1,13 @@
 class Node(object):
     ch = ""
     freq = 0
-    code = None
+    code = ''
     left = None
     right = None
 
     def __init__(self):
-        self.right = None
         self.left = None
+        self.right = None
 
     def addData(self, ch, freq):
         self.ch = ch
@@ -16,8 +16,14 @@ class Node(object):
     def addLeft(self, left):
         self.left = left
 
+    def getLeft(self):
+        return self.left
+
     def addRight(self, right):
         self.right = right
+
+    def getRight(self):
+        return self.right
 
     def getFreq(self):
         return self.freq
@@ -25,18 +31,26 @@ class Node(object):
     def getCh(self):
         return self.ch
 
+    def addCode(self, code):
+        self.code = code
+
+    def getCode(self):
+        return self.code
+
     def printData(self):
         print(self.ch, self.freq)
 
 
 def search_min(dict_obj, min_freq):
+    min_node = None
     for ch, node in dict_obj.items():
         freq = node.getFreq()
         if(freq < min_freq):
             key = ch
             min_freq = freq
+            min_node = node
     dict_obj.pop(key)
-    return node, key, min_freq
+    return min_node, key, min_freq
 
 
 def dictChInDictObject(dict_of_ch):
@@ -55,9 +69,42 @@ def mergeNode(left, right):
     return node
 
 
+def treeTraversal(root, code, encode_dict):
+    if(root == None):
+        return
+    root.addCode(code)
+    treeTraversal(root.getLeft(), code + '1', encode_dict)
+    treeTraversal(root.getRight(), code + '0', encode_dict)
+    if(checkChild(root)):
+        ch = root.getCh()
+        encode_dict[ch] = code
+
+
+def checkChild(root):
+    if(root.getLeft() == None and root.getRight() == None):
+        return 1
+    else:
+        return 0
+
+
+def printTreeCode(root):
+    if(root == None):
+        return
+    print(root.getCh(), root.getCode())
+    printTreeCode(root.getLeft())
+    printTreeCode(root.getRight())
+
+
+def printEncodedText(text, encode_dict):
+    encoded_text = ''
+    for ch in text:
+        encoded_text += encode_dict[ch]
+    print("Encoded text:", encoded_text)
+
+
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 text = input()
-list_of_ch = [0] * 26
+code = ''
 dict_of_ch = {}
 unique_ch_cntr = 0
 for ch in alphabet:
@@ -65,6 +112,9 @@ for ch in alphabet:
     if(cntr):
         dict_of_ch[ch] = cntr
         unique_ch_cntr += 1
+if(unique_ch_cntr == 0):
+    print("Empty text")
+    exit()
 temp_dict = dict_of_ch.copy()
 max_freq = len(text)
 
@@ -75,27 +125,24 @@ dict_obj = dictChInDictObject(dict_of_ch)
 temp_cntr = unique_ch_cntr - 1
 
 for i in range(temp_cntr):
-    node = Node()
     ch = ""
     freq = 0
     min_node_left, left_key, left_freq = search_min(dict_obj, max_freq)
     min_node_right, right_key, right_freq = search_min(dict_obj, max_freq)
     ch = ch + left_key + right_key
     freq += left_freq + right_freq
-    node.addLeft(min_node_left)
-    node.addRight(min_node_right)
+    node = mergeNode(min_node_left, min_node_right)
     node.addData(ch, freq)
     dict_obj.setdefault(ch, node)
-    print('left:', left_key, 'right:', right_key)
-    node.printData()
 
-# try:
-#    dict_obj.popitem()
-# except KeyError:
-#    print("dict is empty")
-# else:
-#    print("succesful")
-# for index in range(26):
-#    val = list_of_ch[index]
-#    if(val):
-#        print(chr(97 + index),' - ', val)
+key, root = dict_obj.popitem()
+encode_dict = {}
+if(checkChild(root)):
+    root.addCode('0')
+    encode_dict[root.getCh()] = '0'
+else:
+    treeTraversal(root, code, encode_dict)
+# print("Tree Print")
+# printTreeCode(root)
+print(encode_dict)
+printEncodedText(text, encode_dict)
