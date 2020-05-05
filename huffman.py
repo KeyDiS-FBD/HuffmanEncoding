9d77c11355de7babb6c404afb357281b30a09647
KeyDiS <KeyDiS-FBD@github.com>
2020-05-05 13:05:08 +0600

    add merge two nodes

--- huffman.py
+++ huffman.py
@@ -1,11 +1,15 @@
 class Node(object):
-    ch = None
-    freq = None
+    ch = ""
+    freq = 0
     code = None
     left = None
     right = None
-    def __init__(self, ch, freq):
-         
+
+    def __init__(self):
+        self.right = None
+        self.left = None
+
+    def addData(self, ch, freq):
         self.ch = ch
         self.freq = freq
 
@@ -17,11 +21,13 @@ class Node(object):
 
     def getFreq(self):
         return self.freq
+
     def getCh(self):
         return self.ch
 
     def printData(self):
         print(self.ch, self.freq)
+
 
 def search_min(dict_obj, min_freq):
     for ch, node in dict_obj.items():
@@ -30,18 +36,20 @@ def search_min(dict_obj, min_freq):
             key = ch
             min_freq = freq
     dict_obj.pop(key)
-    return key, min_freq
+    return node, key, min_freq
 
 
 def dictChInDictObject(dict_of_ch):
     dict_obj = {}
     for ch, freq in dict_of_ch.items():
-        node = Node(ch, freq)
+        node = Node()
+        node.addData(ch, freq)
         dict_obj[ch] = node
     return dict_obj
 
+
 def mergeNode(left, right):
-    node = Node(None, None)
+    node = Node()
     node.addLeft(left)
     node.addRight(right)
     return node
@@ -60,20 +68,34 @@ for ch in alphabet:
 temp_dict = dict_of_ch.copy()
 max_freq = len(text)
 
-#for index in range(26):
-#    cntr = text.count(chr(97 + index))
-#    if(cntr):
-#        list_of_ch.insert(index, cntr)
+
 print(dict_of_ch)
 dict_obj = dictChInDictObject(dict_of_ch)
 
+temp_cntr = unique_ch_cntr - 1
 
-for i in range(unique_ch_cntr):
-    key, freq = search_min(dict_obj, max_freq)
-    print(key, freq)
-print(dict_obj)
-#for index in range(26):
+for i in range(temp_cntr):
+    node = Node()
+    ch = ""
+    freq = 0
+    min_node_left, left_key, left_freq = search_min(dict_obj, max_freq)
+    min_node_right, right_key, right_freq = search_min(dict_obj, max_freq)
+    ch = ch + left_key + right_key
+    freq += left_freq + right_freq
+    node.addLeft(min_node_left)
+    node.addRight(min_node_right)
+    node.addData(ch, freq)
+    dict_obj.setdefault(ch, node)
+    print('left:', left_key, 'right:', right_key)
+    node.printData()
+
+# try:
+#    dict_obj.popitem()
+# except KeyError:
+#    print("dict is empty")
+# else:
+#    print("succesful")
+# for index in range(26):
 #    val = list_of_ch[index]
 #    if(val):
 #        print(chr(97 + index),' - ', val)
-
