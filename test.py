7fe600ea7bd2585a965ffb9539d25e1c750a77ae
Daniyar Kabiden <KeyDiS@localhost.localdomain>
2020-05-11 10:22:27 +0600

    create the file of random characters with line len 80 and dot at the end

--- test.py
+++ test.py
@@ -3,9 +3,16 @@ import random
 file = open('text.txt', 'w')
 text = ''
 num = int(input("Enter number: "))
-for i in range(num):
+
+for i in range(num // 80):
+    for j in range(80):
+        ch = random.randrange(97,123, 1)
+        text += chr(ch)
+    text += '\n'
+
+for i in range(num % 80):
     ch = random.randrange(97,123, 1)
     text += chr(ch)
-
-file.write(text + '.')
+text+= '.'
+file.write(text)
 file.close()
